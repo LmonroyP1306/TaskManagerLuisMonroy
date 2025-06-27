cec2f43db666751cb505a55d5f2c530180f8958a
LuisThona <nmonroy@thonaseguros.mx>
2025-06-27 08:52:15 -0600

    Delete SetInitializer in the Golbal.asax.cs file

--- Global.asax.cs
+++ Global.asax.cs
@@ -14,7 +14,6 @@ namespace TaskManagerLuisMonroy
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            System.Data.Entity.Database.SetInitializer(new TaskManager.Data.DbInitializer());
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
